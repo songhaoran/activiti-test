722f70177a1fb5b221ed1b594a1581e2d740bef2
songhaoran <654644141@qq.com>
2017-08-24 17:41:56 +0800

    Initial commit

---
+++ src/main/java/com/song/web/controller/IndexController.java
@@ -0,0 +1,15 @@
+package com.song.web.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by Song on 2017/8/24.
+ */
+@Controller
+public class IndexController {
+
+   /* @RequestMapping(value = {"index","","/"})
+    public String index() {
+        return "index";
+    }*/
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,6 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+<a href="/activiti-test/workflow/model/create?name=song&key=ran&description=test">activiti-modele-index</a>
+</body>
+</html>
